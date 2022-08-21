8e14f6acdfd7b1d6a41df51a39d572b384d02ab3
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2022-08-21 20:17:01 +0300

    Update onLoadMoreButtonClick.js

--- src/js/onLoadMoreButtonClick.js
+++ src/js/onLoadMoreButtonClick.js
@@ -1,25 +1,30 @@
-import { getGallery } from "./getGallery";
+import { getGallery, params } from "./getGallery";
 import { gallery, createMarkup, addMarkup, clearGallery } from "./galleryOperations";
 import { gallerySimpleLightbox } from "../index";
 import Notiflix from "notiflix";
 
 const loadMoreBtn = document.querySelector('.load-more');
+
 export default async function onLoadMoreButtonClick() {
     const galleryData = await getGallery();
-
-    if (galleryData.hits.length === 0) {
-      loadMoreBtn.classList.add('visually-hidden');
-      Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`);
-      return;
-    }
-   
     const markupArray = galleryData.hits.map(createMarkup);
     addMarkup(markupArray.join(''));
     const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
     window.scrollBy({
         top: cardHeight * 2,
         behavior: "smooth",
-    });
-    gallerySimpleLightbox.refresh();
+    }); 
+    
+    gallerySimpleLightbox.refresh();  
+    const totalPages = Math.ceil(galleryData.totalHits/params.per_page);
+    // console.log("totalPages", totalPages);
+    // console.log("params.page", params.page);
+      if (params.page >= totalPages){
+      
+        loadMoreBtn.classList.add('visually-hidden');
+        Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`)
+      }
+    
+    
 }
 
